4dbed2de5521480e7b15c224e76d0b42542c4ad0
Jt_trịnh <89924574+iamnhatjt@users.noreply.github.com>
2025-02-04 06:43:19 +0700

    api: create a product

--- nestjs/src/modules/product/product.module.ts
+++ nestjs/src/modules/product/product.module.ts
@@ -4,9 +4,14 @@ import { ProductEntity } from './entities/product.entity';
 import { AmountProductEntity } from '../shopping-card/entities/amount-product.entity';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { StorageService } from '../storage/storage.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity, AmountProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity, AmountProductEntity]),
+    StorageModule,
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
