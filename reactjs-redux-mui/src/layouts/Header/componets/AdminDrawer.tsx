c69ae242a791378bd94471832a81a0bb25545834
Jt_trịnh <89924574+iamnhatjt@users.noreply.github.com>
2025-02-15 11:27:15 +0700

    development: react buddy

---
+++ reactjs-redux-mui/src/layouts/Header/componets/AdminDrawer.tsx
@@ -0,0 +1,14 @@
+import useToggle from "../../../hooks/useToggle";
+
+
+const AdminDrawer = () => {
+    const [isShow, onShow, onHide] = useToggle(false)j;
+
+    return (
+        <></>
+    )
+
+
+}
+
+export React.memo(AdminDrawer)
